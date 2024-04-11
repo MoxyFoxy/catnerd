74e3fed2812800a6acc689b485f401a8dd497809
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-04-11 16:11:01 +0100

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,14 +1,22 @@
 {
   description = "Soothing pastel theme for Nix";
 
-  outputs = _:
-  let
-    pkgs = {
-      catppuccin-rofi = (builtins.callPackage ./packages/catppuccin/rofi {});
+  inputs = {
+    nixpkgs = { 
+      url = "github:NixOS/nixpkgs/nixos-unstable";
     };
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+
+    pkgs = nixpkgs.legacyPackages.${system}.extend (self: super: {
+      catppuccin-rofi = self.callPackage ./packages/catppuccin/rofi {};
+    });
   in
   {
-    homeManagerModules.catppuccin = import ./modules/home-manager { inherit pkgs;};
-    nixosModules.catppuccin = import ./modules/nixos { inherit pkgs; };
+    homeManagerModules.catppuccin = import ./modules/home-manager {inherit pkgs; };
+    nixosModules.catppuccin = import ./modules/nixos {inherit pkgs; };
   };
 }
