9e4dc35b8fad73003de748e8dd213a73e80d1c8f
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-05-25 15:47:36 +0100

    feat: :sparkles: added dark mode setting in dconf

--- modules/home-manager/gtk.nix
+++ modules/home-manager/gtk.nix
@@ -49,6 +49,16 @@ in
     };
   };
 
+  #TODO dark mode should be toggled based on flavour
+  config.dconf = {
+    enable = true;
+    settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
+    };
+  };
+
   # config.xdg.configFile = lib.mkIf config.catnerd.enable {
   #   "gtk-4.0/assets".source = "${theme.package}/share/themes/${theme.name}/gtk-4.0/assets";
   #   "gtk-4.0/gtk.css".source = "${theme.package}/share/themes/${theme.name}/gtk-4.0/gtk.css";
