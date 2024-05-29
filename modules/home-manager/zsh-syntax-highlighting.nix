e68c550a9470110df42aad12f1d155e4fe2a41ac
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-05-29 16:44:26 +0100

    fix: :bug: fix typo in zsh-syntax-highlighting

--- modules/home-manager/zsh-syntax-highlighting.nix
+++ modules/home-manager/zsh-syntax-highlighting.nix
@@ -8,7 +8,7 @@ let
   flavour = config.catnerd.flavour;
 in
 {
-  zsh.initExtra = lib.mkIf config.catnerd.enable ''
+  programs.zsh.initExtra = lib.mkIf config.catnerd.enable ''
     source ${pkgs.catppuccin-zsh-syntax-highlighting.override { inherit flavour; }}
   '';
 }
